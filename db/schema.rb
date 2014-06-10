6005e62d63f986c5d663c6d0b260f5369fb56e0c
Jacinda Zhong <jacindaz@gmail.com>
2014-06-10 11:16:52 -0400

    Creates 2 migrations, checkouts and removes favorites
     - adds migrations to create a checkout table
     - adds migration to remote favorites column
     - adds validations in Book and Checkout class

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140610140742) do
+ActiveRecord::Schema.define(version: 20140610145330) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "books", force: true do |t|
-    t.string "title",  null: false
-    t.string "author", null: false
+    t.string  "title",  null: false
+    t.string  "author", null: false
+    t.integer "rating"
+  end
+
+  create_table "checkouts", force: true do |t|
+    t.integer  "book_id",    null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
